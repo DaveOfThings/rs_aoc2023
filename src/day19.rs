69da7c43e843ebf990b9cd1c9ffd0db32f94515c
David Wheeler <dwheeler20874@gmail.com>
2024-01-23 17:44:40 -0500

    Work in progress Day 19.

--- src/day19.rs
+++ src/day19.rs
@@ -1,12 +1,71 @@
+use std::collections::HashMap;
+
 use crate::day::{Day, Answer};
 
+enum Condition {
+    XLt(usize),
+    XGt(usize),
+    MLt(usize),
+    MGt(usize),
+    ALt(usize),
+    AGt(usize),
+    SLt(usize),
+    SGt(usize),
+}
+
+enum Action {
+    Accept,
+    Reject,
+    Continue(String),
+}
+
+impl Action {
+    pub fn from_str(s: &str) -> Option<Action> {
+        // TODO
+    }
+}
+
+struct WorkFlow {
+    steps: Vec<(Condition, Action)>,
+}
+
+impl WorkFlow {
+    pub fn from_str(s: &str) -> Option<WorkFlow> {
+        // TODO
+    }
+}
+
+struct Part {
+    x: usize,
+    m: usize,
+    a: usize,
+    s: usize,
+}
+
+impl Part {
+    pub fn from_str(s: &str) -> Option<Part> {
+        // TODO
+    } 
+}
+
+struct Input {
+    workflows: HashMap<String, WorkFlow>,
+    parts: Vec<Part>,
+}
+
+impl Input {
+    pub fn read(filename: &str) -> Input {
+        // TODO
+    }
+}
+
 pub struct Day19<'a> {
-    _input_filename: &'a str,
+    input_filename: &'a str,
 }
 
 impl<'a> Day19<'a> {
     pub const fn new(filename: &'a str) -> Self {
-        Self { _input_filename: filename }
+        Self { input_filename: filename }
     }
 }
 
@@ -17,5 +76,17 @@ impl<'a> Day for Day19<'a> {
 
     fn part2(&self) -> Answer {
         Answer::None
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::day19::Input;
+
+    #[test]
+    fn test_input() {
+        let input = Input::read("examples/day19_example1.txt");
+        assert_eq!(input.workflows.len(), 11);
+        assert_eq!(input.parts.len(), 5);
     }
 }
