4f0f10c3867e2c1ae784adabc7f98eb1372448d5
David Wheeler <dwheeler20874@gmail.com>
2024-11-29 20:54:16 -0500

    Work in progress on Day21, part 2.

--- src/day21.rs
+++ src/day21.rs
@@ -1,4 +1,4 @@
-use std::{collections::{HashMap, HashSet}, fs::File, io::{BufRead, BufReader}};
+use std::{cmp, collections::HashSet, fs::File, io::{BufRead, BufReader}};
 
 use crate::day::{Day, Answer};
 
@@ -19,17 +19,15 @@ impl Input {
         let f = File::open(filename).unwrap();
         let reader = BufReader::new(f);
 
-        let mut y = 0;
-        for line in reader.lines() {
+        for (y, line) in reader.lines().enumerate() {
             let line = line.unwrap();
             width = line.len() as isize;
-            let mut x = 0;
-            for c in line.chars() {
+            for (x, c) in line.chars().enumerate() {
                 match c {
                     '.' => {
                         // garden patch
                         // println!("Inserting at ({x}, {y})");
-                        garden.insert((x, y));
+                        garden.insert((x as isize, y as isize));
                     }
                     '#' => {
                         // rock
@@ -37,17 +35,15 @@ impl Input {
                     }
                     'S' => {
                         // start position (garden patch)
-                        start = (x, y);
-                        garden.insert((x, y));
+                        start = (x as isize, y as isize);
+                        garden.insert((x as isize, y as isize));
                     }
                     _ => {
                         // Something illegal.
                         panic!();
                     }
                 }
-                x += 1;
             }
-            y += 1;
             height += 1;
         }
 
@@ -66,67 +62,70 @@ impl<'a> Day21<'a> {
         Self { input_filename: filename }
     }
 
+/*
+    fn tile_occupied(input: &Input, start: (isize, isize), steps: usize) -> usize
+    {
+        // compute max number of steps we'll need to take.  If asked for more than this
+        // we'll truncate to this while keeping parity.
+        let max_x = cmp::max(start.0-1, input.width-start.0-1);
+        let max_y = cmp::max(start.1-1, input.height-start.1-1);
+        let max_steps = max_x + max_y;
+
+        0  // TODO-DW
+    }
+*/
+
     fn num_by_steps(steps: usize, input: &Input) -> usize {
-        let mut aged_out: usize = 0;
-
         // We're going to do let 'current' and 'next' swap between these maps.
-        let mut map1: HashMap<(isize, isize), usize> = HashMap::new();
-        let mut map2: HashMap<(isize, isize), usize> = HashMap::new();
+        let mut set1: HashSet<(isize, isize)> = HashSet::new();
+        let mut set2: HashSet<(isize, isize)> = HashSet::new();
 
-        let current = &mut map1;
-        let next = &mut map2;
+        let current = &mut set1;
+        let next = &mut set2;
 
         // start position is the only occupied space, initially.
-        current.insert(input.start, 1);  
+        current.insert(input.start);  
 
         for _ in 0..steps {
             // clear out occupancy of next before we start
             next.clear();
 
-            for ((x, y), t) in current.iter() {
-                // If this position has been on for N time ticks ...
-                match t {
-                    1 => {
-                        // Check all neighbors.  If off, turn them on for next iteration.
-                        for (nx, ny) in [((*x+1), *y), (*x, (*y+1)), ((*x-1), *y), (*x, (*y-1))] {
-                            let check_x = nx.rem_euclid(input.width);
-                            let check_y = ny.rem_euclid(input.height);
-        
-                            // if (nx, ny) is in garden set and it's not active, activate for next cycle
-                            if input.garden.contains(&(check_x, check_y)) {
-                                if !current.contains_key(&(nx, ny)) {
-                                    // turn  (nx, ny) on for the first time.
-                                    next.insert((nx, ny), 1);
-                                }
-                                else {
-                                    // a neighbor is occupied, that will keep the lights on for this square for a second tick.
-                                    next.insert((*x, *y), 2);
-                                    println!("({}, {}) is on for two ticks.", *x, *y);
-                                    assert!(false);
-                                }
-                            }
-                        }
+            for (x, y) in current.iter() {
+                // Check all neighbors.  If off, turn them on for next iteration.
+                for (nx, ny) in [((*x+1), *y), (*x, (*y+1)), ((*x-1), *y), (*x, (*y-1))] {
+                    let check_x = nx.rem_euclid(input.width);
+                    let check_y = ny.rem_euclid(input.height);
+
+                    // if (nx, ny) is in garden set put it in the next cycle
+                    if input.garden.contains(&(check_x, check_y)) {
+                        next.insert((nx, ny));
                     }
-                    2 => {
-                        // All neighbors should already be on for the last 1 tick.  This position will
-                        // age to 3 ticks.
-                        next.insert((*x, *y), 3);
-                    }
-                    _ => {
-                        // let permanently on squares that are 3 or more ticks old be dropped.  They
-                        // shouldn't affect any future cells as those cells will now be 2 or more ticks
-                        // old and stuck on.  We do need to keep count of these that have dropped, though.
-                        aged_out += 1;
-                    }
-                }
-                
+                }                
             }
 
             // set occupied to next
             std::mem::swap(current, next);
         }
 
-        let num_occupied = &current.len() + aged_out;
+        let num_occupied = current.len();
+
+        /*
+        for y in 0..input.height {
+            for x in 0..input.width {
+                if current.contains(&(x, y)) {
+                    print!("O");
+                }
+                else if input.garden.contains(&(x, y)) {
+                    print!(".");
+                }
+                else {
+                    print!("#");
+                }
+            }
+            println!();
+        }
+        println!();
+        */
 
         num_occupied
     
@@ -165,12 +164,12 @@ mod test {
     fn test_num_by_steps() {
         let input = Input::read("examples/day21_example1.txt");
 
-        let cases: [(usize, usize); 5] = [
+        let cases: [(usize, usize); 4] = [
             (6, 16), 
             (10, 50), 
             (50, 1594),
             (100, 6536), 
-            (500, 167004), 
+            // (500, 167004), 
             //(1000, 668697), 
             // (5000, 16733044)
             ];
@@ -178,13 +177,5 @@ mod test {
             let reach = Day21::num_by_steps(*steps, &input);
             assert_eq!(reach, *expected);
         }
-    }
-
-    #[test]
-    fn test_mod() {
-        for n in -5..5isize {
-            println!("{n} rem_euclid 3 is {}", n.rem_euclid(3));
-        }
-        assert!(true);
     }
 }
