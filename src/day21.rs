f40f5979a38a3a6ae4e4def5bd45dfa13d67df6e
David Wheeler <daveofthings@gmail.com>
2025-10-09 14:54:18 -0400

    Working on Day 21, Part 2.

--- src/day21.rs
+++ src/day21.rs
@@ -53,6 +53,45 @@ impl Input {
 
 }
 
+enum Cases {
+    // Start in the center
+    Center_Even,
+    Center_Odd,
+
+    // Enter on CL, propagating north
+    North_Even,
+    North_Odd,
+    North(usize), // last and 2nd to last tiles
+
+    // Enter on corner, propagating NE
+    NorthEast_Even,  
+    NorthEast_Odd,
+    NorthEast(usize),
+
+    // ...
+
+    // Enter on CL, propagating East
+    East_Even,
+    East_Odd,
+    East(usize), // last and 2nd to last tiles
+
+    // ...
+
+    // Enter on CL, propagating South
+    South_Even,
+    South_Odd,
+    South(usize), // last and 2nd to last tiles
+
+    // ...
+
+    // Enter on CL, propagating West
+    West_Even,
+    West_Odd,
+    West(usize), // last and 2nd to last tiles
+
+    // ...
+}
+
 pub struct Day21<'a> {
     input_filename: &'a str,
 }
@@ -74,6 +113,76 @@ impl<'a> Day21<'a> {
         0  // TODO-DW
     }
 */
+
+    fn num_by_tile_analysis(steps: usize, input: &Input) -> usize {
+        let steps = steps as isize;  // just simplify all the signed/unsigned stuff
+        let to_cross_center= (input.width+1)/2;
+        let full_tiles_crossed = (steps-to_cross_center)/input.width;
+        let cl_last_steps = (steps - to_cross_center) % input.width;
+        let cl_2nd_last_steps = cl_last_steps + input.width;
+
+        println!("Analyzing for {} steps, tiles are {}x{}", steps, input.width, input.height);
+
+        println!("Will cross center plus {} full tiles plus {} steps", full_tiles_crossed, cl_last_steps);
+
+        // Last CL tile
+        println!("There are 4 last CL tiles, eval at {}", cl_last_steps);
+
+        // Second to CL edge tile
+        println!("There are 4 2nd last CL tiles, eval at {}", cl_last_steps+input.width);
+
+        // Steps to simulate on center tile
+        let center_steps = if (steps & 1) == 1 
+            { 2*to_cross_center+1 } // for odd #steps
+            else { 2*to_cross_center }; // for even #steps
+        println!("There is 1 central tile, full in {} steps, eval at {}", 2*to_cross_center, center_steps);
+
+        // Odd-step and even-step CL tiles
+        let cl_3d_last_steps = cl_2nd_last_steps + input.width;
+        let cl_bulk_tiles = full_tiles_crossed - 1;  // because 2nd to last is special
+        let (num_cl_odd, num_cl_even) = 
+            if (cl_3d_last_steps & 1) == 0 {
+                // The furthest full CL tile will experience an even number of steps after initial contact.
+                if ((steps - to_cross_center) & 1) == 0 {
+                    // The first CL square will experience an even number of steps
+                    // So there will be one more even tile than odd ones
+                    (cl_bulk_tiles / 2, cl_bulk_tiles / 2 + 1)
+                }
+                else {
+                    // Equal numbers of even and odd step tiles.
+                    (cl_bulk_tiles / 2, cl_bulk_tiles / 2)
+                }
+            }
+            else {
+                if ((steps - to_cross_center) & 1) == 0 {
+                    // The first CL square will experience an even number of steps
+                    // So there will be one more even tile than odd ones
+                    (cl_bulk_tiles / 2, cl_bulk_tiles / 2)
+                }
+                else {
+                    // Equal numbers of even and odd step tiles.
+                    (cl_bulk_tiles / 2, cl_bulk_tiles / 2 + 1)
+                }
+            };
+
+        println!("The center line will have {} tiles with even counts, {} with odd.", num_cl_even, num_cl_odd);
+
+        // Furthest diagonal tiles
+        let num_furthest_diag = if cl_last_steps >= self.height/2 {
+            // The furthest diagonal will be in same col as furthest CL tile
+            full_tiles_crossed###
+        }
+        else {
+            // The furthest diagonal will be one col short of furthest CL tile.
+
+        }
+        // Next furthest diagonal tiles
+        // Third furthest diagonal tiles
+        // Bulk tiles, odd-steps
+        // Bulk tiles, even steps
+
+        0 // TODO
+    }
 
     fn num_by_steps(steps: usize, input: &Input) -> usize {
         // We're going to do let 'current' and 'next' swap between these maps.
@@ -164,12 +273,12 @@ mod test {
     fn test_num_by_steps() {
         let input = Input::read("examples/day21_example1.txt");
 
-        let cases: [(usize, usize); 4] = [
+        let cases= [
             (6, 16), 
             (10, 50), 
             (50, 1594),
             (100, 6536), 
-            // (500, 167004), 
+            (500, 167004), 
             //(1000, 668697), 
             // (5000, 16733044)
             ];
@@ -177,5 +286,19 @@ mod test {
             let reach = Day21::num_by_steps(*steps, &input);
             assert_eq!(reach, *expected);
         }
+    }
+
+    #[test]
+    fn test_by_analysis() {
+        let input = Input::read("examples/day21_example1.txt");
+
+        let n = Day21::num_by_tile_analysis(5000, &input);
+    }
+
+    #[test]
+    fn test_by_analysis2() {
+        let input = Input::read("data_aoc2023/day21.txt");
+
+        let n = Day21::num_by_tile_analysis(26501365, &input);
     }
 }
